9d5b2dca28f991bb0cc45050531b6e4c442e383d
JaviSanLop <javier.s358268@cesurformacion.com>
2024-05-10 10:14:24 +0200

    Subida proyecto pokemon

---
+++ ProyectoPokemon/src/bbdd/usuarioTest.java
@@ -0,0 +1,14 @@
+package bbdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class usuarioTest {
+
+	@Test
+	void testLogin() {
+		fail("Not yet implemented");
+	}
+
+}
