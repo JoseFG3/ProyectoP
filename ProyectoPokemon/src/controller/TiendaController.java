e4b972e407b27ed031a2f0030cfb3f57b2f6e6bb
JoseFG3 <empirexxonio@gmail.com>
2024-05-14 18:52:24 +0200

    tienda

---
+++ ProyectoPokemon/src/controller/TiendaController.java
@@ -0,0 +1,123 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+public class TiendaController implements Initializable{
+
+    @FXML
+    private Button btnAnillo;
+
+    @FXML
+    private Button btnBaston;
+
+    @FXML
+    private Button btnChaleco;
+
+    @FXML
+    private Button btnPesa;
+
+    @FXML
+    private Button btnPila;
+
+    @FXML
+    private Button btnPluma;
+
+    @FXML
+    private Button btnPokedex;
+
+    @FXML
+    private Button btnReturn;
+
+    @FXML
+    void comprarAnillo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarBaston(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarChaleco(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarPesa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarPila(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarPluma(ActionEvent event) {
+
+    }
+
+    @FXML
+    void comprarPokedex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irMenu(ActionEvent event) {
+    	loadStage("../view/MENU-SCENE.fxml", event);
+    }
+    private void loadStage(String url, Event event) {
+        try {
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene oldScene = sourceAsNode.getScene();
+            Window window = oldScene.getWindow();
+            Stage stage = (Stage) window;
+            stage.hide();
+
+            Parent root = FXMLLoader.load(getClass().getResource(url));
+            Scene scene = new Scene(root);
+
+            Stage newStage = new Stage();
+            newStage.setScene(scene);
+            newStage.show();
+
+            newStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    Platform.exit();
+                }
+            });
+
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
