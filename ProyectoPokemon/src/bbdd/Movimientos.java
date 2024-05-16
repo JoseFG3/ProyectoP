182b131c67be75167eeaa5c2125257a77780f6d0
JaviSanLop <javier.s358268@cesurformacion.com>
2024-05-16 20:20:16 +0200

    Actualizacion de Base de datos

--- ProyectoPokemon/src/bbdd/Movimientos.java
+++ ProyectoPokemon/src/bbdd/Movimientos.java
@@ -5,24 +5,12 @@ public class Movimientos {
     private String nomMovimiento;
     private int potencia;
     private String tipo;
-    private String estado;
-    private int quita;
-    private int turnos;
-    private String mejora;
-    private int cantMejora;
-    private int nivelAprendizaje;
 
-    public Movimientos(int idMovimiento, String nomMovimiento, int potencia, String tipo, String estado, int quita, int turnos, String mejora, int cantMejora, int nivelAprendizaje) {
+    public Movimientos(int idMovimiento, String nomMovimiento, int potencia, String tipo) {
         this.idMovimiento = idMovimiento;
         this.nomMovimiento = nomMovimiento;
         this.potencia = potencia;
         this.tipo = tipo;
-        this.estado = estado;
-        this.quita = quita;
-        this.turnos = turnos;
-        this.mejora = mejora;
-        this.cantMejora = cantMejora;
-        this.nivelAprendizaje = nivelAprendizaje;
     }
 
     // Getters y setters
@@ -56,53 +44,5 @@ public class Movimientos {
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getQuita() {
-        return quita;
-    }
-
-    public void setQuita(int quita) {
-        this.quita = quita;
-    }
-
-    public int getTurnos() {
-        return turnos;
-    }
-
-    public void setTurnos(int turnos) {
-        this.turnos = turnos;
-    }
-
-    public String getMejora() {
-        return mejora;
-    }
-
-    public void setMejora(String mejora) {
-        this.mejora = mejora;
-    }
-
-    public int getCantMejora() {
-        return cantMejora;
-    }
-
-    public void setCantMejora(int cantMejora) {
-        this.cantMejora = cantMejora;
-    }
-
-    public int getNivelAprendizaje() {
-        return nivelAprendizaje;
-    }
-
-    public void setNivelAprendizaje(int nivelAprendizaje) {
-        this.nivelAprendizaje = nivelAprendizaje;
     }
 }
